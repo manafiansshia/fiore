f950669210535f2ecf7864c1d74f143498f9a5a6
manafiansshia <136214749+manafiansshia@users.noreply.github.com>
2023-06-30 14:23:17 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,55 @@
+const hamaburger = document.querySelector(".hamburger-menu-box");
+const nav = document.querySelector(".hamburger-nav");
+const body = document.querySelector("body");
+const hamaburgerMenu = document.querySelector(".hamburger-menu-content");
+// フェードイン
+
+
+
+const navItem1 = document.querySelector(".item1");
+const navItem2 = document.querySelector(".item2");
+const navItem3 = document.querySelector(".item3");
+const navItem4 = document.querySelector(".item4");
+const navItem5 = document.querySelector(".item5");
+
+hamaburger.addEventListener("click",()=> {
+    hamaburger.classList.toggle("is-active");
+    nav.classList.toggle("is-active");
+    body.classList.toggle("is-active");
+    hamaburgerMenu.classList.toggle("is-active");
+});
+
+
+navItem1.addEventListener("click",()=> {
+    hamaburger.classList.toggle("is-active");
+    nav.classList.toggle("is-active");
+    body.classList.toggle("is-active");
+    hamaburgerMenu.classList.toggle("is-active");
+});
+navItem2.addEventListener("click",()=> {
+    hamaburger.classList.toggle("is-active");
+    nav.classList.toggle("is-active");
+    body.classList.toggle("is-active");
+    hamaburgerMenu.classList.toggle("is-active");
+});
+navItem3.addEventListener("click",()=> {
+    hamaburger.classList.toggle("is-active");
+    nav.classList.toggle("is-active");
+    body.classList.toggle("is-active");
+    hamaburgerMenu.classList.toggle("is-active");
+});
+navItem4.addEventListener("click",()=> {
+    hamaburger.classList.toggle("is-active");
+    nav.classList.toggle("is-active");
+    body.classList.toggle("is-active");
+    hamaburgerMenu.classList.toggle("is-active");
+});
+navItem5.addEventListener("click",()=> {
+    hamaburger.classList.toggle("is-active");
+    nav.classList.toggle("is-active");
+    body.classList.toggle("is-active");
+    hamaburgerMenu.classList.toggle("is-active");
+});
+
+
+
